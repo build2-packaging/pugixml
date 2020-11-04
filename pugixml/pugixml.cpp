8393537e54c3a492496e7d6c26c001feada5750e
Joël Lamotte <joel@jellynote.com>
2020-11-04 14:38:18 +0100

    Fixes file type changes (to links)

--- pugixml/pugixml.cpp
+++ pugixml/pugixml.cpp
@@ -1,17 +1,1 @@
-#include <pugixml/pugixml.hpp>
-
-#include <ostream>
-#include <stdexcept>
-
-using namespace std;
-
-namespace pugixml
-{
-  void say_hello (ostream& o, const string& n)
-  {
-    if (n.empty ())
-      throw invalid_argument ("empty name");
-
-    o << "Hello, " << n << '!' << endl;
-  }
-}
+../upstream/src/pugixml.cpp
--- pugixml/pugixml.hpp
+++ pugixml/pugixml.hpp
@@ -1,15 +1,1 @@
-#pragma once
-
-#include <iosfwd>
-#include <string>
-
-#include <pugixml/export.hpp>
-
-namespace pugixml
-{
-  // Print a greeting for the specified name into the specified
-  // stream. Throw std::invalid_argument if the name is empty.
-  //
-  PUGIXML_SYMEXPORT void
-  say_hello (std::ostream&, const std::string& name);
-}
+../upstream/src/pugixml.hpp
